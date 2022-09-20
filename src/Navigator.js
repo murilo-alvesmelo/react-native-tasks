308392c564ceb5d7e74092581b6bfd4702595195
Murilo Alves Melo <murilo.melo72@gmail.com>
2022-09-20 17:20:56 -0300

    Corrigindo autenticação do login, alterando para Reac.Component

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,33 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
 import TaskList from "./screens/TaskList";
 import Auth from "./screens/Auth";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function Authenticate(){
-    return(
-        <Auth/>
-    )
-}
+const Stack = createNativeStackNavigator()
 
-function Home(){
-    return(
-        <TaskList/>
-    )
-}
+class Navigator extends React.Component{
+    render(){
+        return(
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen 
+                        name="Auth" 
+                        component={Auth} 
+                        options={{headerShown: false}}/>
 
-const Stack = createNativeStackNavigator();
-
-function Navigator(){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Auth">
-                <Stack.Screen name="Auth" component={Authenticate} options={{headerShown: false}}/>
-                <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+                    <Stack.Screen 
+                        name="Home" 
+                        component={TaskList} 
+                        options={{headerShown: false}}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        )
+    }
 }
 
 export default Navigator;
